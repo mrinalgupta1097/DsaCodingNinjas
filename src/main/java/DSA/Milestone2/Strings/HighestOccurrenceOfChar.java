0310572a5caa36702db94fa87baff4ade535acf0
Mrinal Gupta <mrinalgupta1097@gmail.com>
2023-07-03 14:52:13 +0530

    highest occurrence of character

--- src/main/java/DSA/Milestone2/Strings/HighestOccurrenceOfChar.java
+++ src/main/java/DSA/Milestone2/Strings/HighestOccurrenceOfChar.java
@@ -2,20 +2,50 @@ package DSA.Milestone2.Strings;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.TreeMap;
 
 public class HighestOccurrenceOfChar {
     public static char highestOccuringChar(String str) {
-        //create a hashmap: key-> char, value-> occurrence
-        Map<Character, Integer> charOcc = new TreeMap<>();
-        int maxOcc = 0;
-        char key = '!';
-        // iterate over string and insert values in map
+        //create a hashmap to store the occurrence of characters
+        Map<Character, Integer> map = new HashMap<>();
+        char c = Character.MIN_VALUE;
+        //iterate over string to find the occurrence count per character
         for (char ch :
                 str.toCharArray()) {
-            charOcc.compute(ch, (k, v) -> (v == null) ? 1 : v++);
+            map.compute(ch, (k, v) -> (v == null) ? 1 : ++v);
         }
-       return 'c';
+        // take a temp max value;
+        int maxOcc = Integer.MIN_VALUE;
+        // query map to fint he key for max value
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            if(entry.getValue() > maxOcc){
+                maxOcc = entry.getValue();
+                c = entry.getKey();
+            }
+        }
+        System.gc();
+        return c;
+    }
+    public static char highestOccuringCharWithoutHashmap(String str) {
+        int[] count = new int[256];
+        int max = 0;
+        char ch = '\0';
+        for (int i = 0; i< str.length(); i++){//aa
+            count[str.charAt(i)]++;
+        }
+        for(int i = 0; i<str.length(); i++){
+            if(count[str.charAt(i)] > max){
+                max = count[str.charAt(i)];
+                ch = str.charAt(i);
+            }
+        }
+        return ch;
+    }
+
+
+
+    public static void main(String[] args) {
+        System.out.println(highestOccuringChar("aabbbbcdddd"));
+        System.out.println(highestOccuringCharWithoutHashmap("aabbbbcdddd"));
 
     }
 }
