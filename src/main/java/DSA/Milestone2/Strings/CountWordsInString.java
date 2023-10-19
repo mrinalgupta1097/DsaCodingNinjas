e12fe787214b57bfeb5c63ddc44004c0c0415921
mrinalgupta <mrinalgupta1097@gmail.com>
2023-10-19 14:00:11 +0530

    refactored

--- src/main/java/DSA/Milestone2/Strings/CountWordsInString.java
+++ src/main/java/DSA/Milestone2/Strings/CountWordsInString.java
@@ -2,6 +2,6 @@ package DSA.Milestone2.Strings;
 
 public class CountWordsInString {
     public static int countWords(String str) {
-        return (str.length() == 0) ? 0 : (str.split(" ")).length;
+        return (str.isEmpty()) ? 0 : (str.split(" ")).length;
     }
 }
